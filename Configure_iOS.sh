3ae9344b7e7ab732f9a0fa99811e9877fda4dd34
Danny de Bruijne <dannydb44@gmail.com>
2022-06-12 14:12:28 +0200

    Prepare for iOS

--- Configure_iOS.sh
+++ Configure_iOS.sh
@@ -1,6 +1,3 @@
 #/bin/bash
-rm -rf build
-mkdir build
-cd build
 export VULKAN_SDK=/Users/danny/VulkanSDK/1.3.211.0/macOS
-cmake .. -G Xcode -DCMAKE_TOOLCHAIN_FILE=../ios.toolchain.cmake -DPLATFORM=OS64
+cmake . -G Xcode -B build -DCMAKE_TOOLCHAIN_FILE=ios.toolchain.cmake -DPLATFORM=OS64 -DENABLE_BITCODE=FALSE -DDEPLOYMENT_TARGET=13
